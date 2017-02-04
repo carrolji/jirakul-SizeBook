6f3a0cc5d73dde28a39f13dcce235e07509a57da
carrolji <jirakul@ualberta.ca>
2017-02-04 14:10:30 -0700

    add notifyDataSetChanged()

--- app/src/main/java/com/example/jirakul_sizebook/MainActivity.java
+++ app/src/main/java/com/example/jirakul_sizebook/MainActivity.java
@@ -112,6 +112,7 @@ public class MainActivity extends Activity {
                         ,neckTxt.getText().toString(),bustTxt.getText().toString()
                 ,chestTxt.getText().toString(),waistTxt.getText().toString(),hipTxt.getText().toString(),inseamTxt.getText().toString()
                 ,commentTxt.getText().toString()));
+                contactAdapter.notifyDataSetChanged();
                 showTotalRecord();
                 saveInFile();
                 Toast.makeText(getApplicationContext(),nameTxt.getText().toString() +" added",Toast.LENGTH_SHORT).show();
