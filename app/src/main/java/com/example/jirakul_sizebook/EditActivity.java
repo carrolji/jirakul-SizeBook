0759eadee015926a7d4ba9f8a0a846802db177b8
carrolji <jirakul@ualberta.ca>
2017-02-04 20:16:28 -0700

    update button

--- app/src/main/java/com/example/jirakul_sizebook/EditActivity.java
+++ app/src/main/java/com/example/jirakul_sizebook/EditActivity.java
@@ -80,6 +80,22 @@ public class EditActivity extends AppCompatActivity {
         commentTxt.setText(oldComment);
 
         saveData.setOnClickListener(saveButtonListener);
+        nameTxt.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                saveData.setEnabled(!nameTxt.getText().toString().trim().isEmpty());
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
 
 
     }
